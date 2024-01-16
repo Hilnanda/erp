d1fcd42f7270d1a1472a42781b54f40392be731c
Hilnanda Ardiansyah <ardikeren75@gmail.com>
2024-01-16 15:35:56 +0700

    ardi/hided login

--- resources/views/layouts/auth2.blade.php
+++ resources/views/layouts/auth2.blade.php
@@ -64,7 +64,7 @@
                     @if(!($request->segment(1) == 'business' && $request->segment(2) == 'register'))
                         <!-- Register Url -->
                         @if(config('constants.allow_registration'))
-                            <a href="{{ route('business.getRegister') }}@if(!empty(request()->lang)){{'?lang=' . request()->lang}} @endif" class="btn bg-maroon btn-flat" ><b>{{ __('business.not_yet_registered')}}</b> {{ __('business.register_now') }}</a>
+                            {{-- <a href="{{ route('business.getRegister') }}@if(!empty(request()->lang)){{'?lang=' . request()->lang}} @endif" class="btn bg-maroon btn-flat" ><b>{{ __('business.not_yet_registered')}}</b> {{ __('business.register_now') }}</a> --}}
                             <!-- pricing url -->
                             @if(Route::has('pricing') && config('app.env') != 'demo' && $request->segment(1) != 'pricing')
                                 &nbsp; <a href="{{ action([\Modules\Superadmin\Http\Controllers\PricingController::class, 'index']) }}">@lang('superadmin::lang.pricing')</a>
